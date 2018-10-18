1e97ade074fa8be44e3ff358650c14062dc1de76
MartinPegoraro <martinpegoraro8@gmail.com>
2018-10-18 10:40:00 -0300

    Ajustes para poder exportar el contrato

--- app.js
+++ app.js
@@ -3,6 +3,156 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var myContract;
+var Web3 = require('web3');
+fs = require('fs')
+solc = require('solc')
+
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var abiDefinition = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "getProfesor",
+      "outputs": [
+          {
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "esConfirmado",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "getMateria",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_profesor",
+              "type": "address"
+          },
+          {
+              "name": "_materia",
+              "type": "string"
+          }
+      ],
+      "name": "solicitar",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "esCancelado",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "key",
+              "type": "address"
+          }
+      ],
+      "name": "confirmar",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [],
+      "name": "cancelar",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "key",
+              "type": "address"
+          }
+      ],
+      "name": "getAlumno",
+      "outputs": [
+          {
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  }
+];
+
+var code = fs.readFileSync('./tutoria.sol').toString()
+
+var compiledCode = solc.compile(code)
+
+var byteCode = compiledCode.contracts[':Tutoria'].bytecode //Su contrato inteligente
+
+var TutoriaContract = new web3.eth.Contract(abiDefinition,{data: byteCode, from: web3.eth.accounts[0], gas: 47000}) 
+
+ TutoriaContract.deploy({data:byteCode})
+.send({from:'0x2af24ec65db90aa75fc9c918da71e498c6812efe',gas: 1500000, gasPrice: '30000'})
+.then(function(newContractInstance){
+    myContract = newContractInstance});
+
+
 
 var blockchainRouter = require('./routes/blockchain');
 
@@ -11,6 +161,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -38,4 +189,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
 module.exports = app;
+module.exports.myContract = myContract;

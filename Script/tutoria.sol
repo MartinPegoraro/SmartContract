92b542bb8e9ee124e12a99e1fb61874976fd8748
MartinPegoraro <martinpegoraro8@gmail.com>
2018-10-28 16:04:36 -0300

    Arreglos final para que el profesor pueda confirmar y el alumno pueda cancelar

--- Script/tutoria.sol
+++ Script/tutoria.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.7;
 contract Tutoria {
     
-    mapping (address => TutoriaData)  Tutorias;
+    mapping (uint256 => TutoriaData)  Tutorias;
     
     
     struct TutoriaData {
@@ -14,57 +14,62 @@ contract Tutoria {
         bytes32 hash;
     }
     
-    function solicitar(string mater, address idProf) public{
-        require(msg.sender != idProf);
-        TutoriaData t = Tutorias[msg.sender];
+    function solicitar(string mater, address idProf) public returns (uint256){
+       
+        uint256 key = uint256(keccak256(abi.encode(msg.sender, idProf, mater, block.timestamp, block.difficulty)));
+
+        TutoriaData t = Tutorias[key];
+        
         t.materia = mater;
         t.idProfesor = idProf;
         t.alumno = msg.sender;
         t.isConfirmado = false;
         t.isCancelado = false;
         t.fecha = block.timestamp;
-        t.hash = keccak256(t.materia,t.idProfesor,t.alumno,t.isConfirmado,t.isCancelado,t.fecha);
+        
+       return key;
+       
     }
     
-    function getFecha(address key) public view returns (uint) {
+    function getFecha(uint256 key) public view returns (uint) {
         return Tutorias[key].fecha;
     }
 
-    function getHash(address key) public view returns (bytes32) {
+    function getHash(uint256 key) public view returns (bytes32) {
         return Tutorias[key].hash;
     }
     
-    function getMateria(address key) public view returns (string) {
+    function getMateria(uint256 key) public view returns (string) {
         return Tutorias[key].materia;
     }
     
-    function getIdProfesor(address key) public view returns (address) {
+    function getIdProfesor(uint256 key) public view returns (address) {
         return Tutorias[key].idProfesor;
     }
     
-    function getAlumno(address key) public view returns (address) {
+    function getAlumno(uint256 key) public view returns (address) {
         return Tutorias[key].alumno;
     }
     
-    function confirmar(address key) public returns (bool) {
-        require(Tutorias[key].idProfesor == msg.sender);
+    function confirmar(uint256 key) public returns (bool) {
         require(Tutorias[key].isConfirmado == false);
         require(Tutorias[key].isCancelado == false);
         return Tutorias[key].isConfirmado = true;
     }
     
-    function cancelar(address key) public returns (bool) {
-        require(Tutorias[key].alumno == msg.sender);
+    function cancelar(uint256 key) public returns (bool) {
         require(Tutorias[key].isConfirmado == false);
         require(Tutorias[key].isCancelado == false);
-        return Tutorias[key].isCancelado = true;
+        TutoriaData t= Tutorias[key];
+        t.isCancelado = true;
+        return t.isCancelado;
     }
     
-    function estaConfirmado(address key) public view returns (bool){
+    function estaConfirmado(uint256 key) public view returns (bool){
         return Tutorias[key].isConfirmado;
     }
     
-    function estaCancelado(address key) public view returns (bool){
+    function estaCancelado(uint256 key) public view returns (bool){
         return Tutorias[key].isCancelado;
     }
     
--- app.js
+++ app.js
@@ -19,12 +19,11 @@ compiledCode = solc.compile(code)
 abiDefinition = JSON.parse(compiledCode.contracts[':Tutoria'].interface) 
 byteCode = compiledCode.contracts[':Tutoria'].bytecode 
 
-
-
 let config = {
   addressContract: null,
   tutos:[]
 };
+
 let alumno= {
   Gonza:[],
   Facundo:[],
@@ -57,22 +56,26 @@ app.post('/', function (req, res) {
   let materia = req.body.materia;
   let profesor = req.body.profesor;
 
+
+  //------Se verifica que el los profesores no puedan solicitar tutorias----------------
   if ((usuario ==prof[0]) || (usuario ==prof[1]) || (usuario ==prof[2])){
     res.send('Profesores no pueden solicitar tutorias')
   }else{
-  //Instancia del Contrato
+  //----------------------Instancia del Contrato------------------------
     myContract = new web3.eth.Contract(abiDefinition, config.addressContract, { data: byteCode, gasPrice: '1000', gas: 200000 });
     myContract.methods.solicitar(materia, profesor).send({ from: usuario, gas: 200000 })
-    myContract.methods.solicitar(materia,profesor).call()
+    myContract.methods.solicitar(materia,profesor).call({from: usuario})
     .then(e => {
-      console.log(e) //Key
+      console.log(e)
 
+      //------------------Se crea arreglo con todas las tutorias----------------------
       config.tutos.push({
         'alumno': usuario,
         'materia': materia,
         'key': e,
-        'profesor': profesor
+        'profesor': profesor,
       })
+      //------------------Se crean los arreglos con tutorias por alumno-----------------
       switch(usuario){
         case "0xfd730bab2d10d2179aec947409e2f0c5d1ac5021":
           alumno.Gonza.push({
@@ -142,21 +145,21 @@ app.get('/metodos', function (req, res) {
 
 //-------------------------METODOS-----------------------------------------
 app.post('/metodos', function (req, res) {
+  //----------Obtengo los datos del HTML mediante POST---------------------
   let usuario = req.body.usuario;
   let metodo = req.body.metodo;
   let opc = req.body.opc;
+  //-----------Arreglos con los ID de los profesores y alumnos-------------
   var prof = ["0x2af24ec65db90aa75fc9c918da71e498c6812efe", "0xb84d66b9aa22c77cb82b756a64dba9e26719b4d7","0xbd7b59c5c01e860150200df323b703228625eff7"];
-  var alum = ["0xfd730bab2d10d2179aec947409e2f0c5d1ac5021", "0xcffd857638c1e8e5dcb8df934f6c2d8fd12fea61", "0xfd8fc5f54264089778fe45d188b1636d9399ce5e", "0xedb0e48927a72e9dc907afd23d9794586acfd149", "0x95f4c7b52bd3890dd6ab2a488d51414b3b961eca", "0xff7cbcb3bae4b13db50860a023e31f4a0b0bfa46", "0xbf965026809eeda78bd15e66ed185220fbc44c0b"]
+  
   switch (metodo) {
+    //----------------------------GET MATERIA---------------------------------
     case "1":
+      if ((usuario in prof)){
+        res.send('Profesor no pueden solicitar materia')
+      }
       myContract.methods.getMateria(usuario).call().then(e => {
         console.log(e)
-        if ((usuario in prof)){
-          res.send('Profesor no pueden solicitar materia')
-        }
-//        if (e.length < 1){
-//          res.send('Tutoria no encontrada')
-//        }
         switch(usuario){
           case "0xfd730bab2d10d2179aec947409e2f0c5d1ac5021":
             res.json(alumno.Gonza)
@@ -182,6 +185,7 @@ app.post('/metodos', function (req, res) {
         }
       });
       break;
+      //--------------------------GET FECHA---------------------
     case "2":
       myContract.methods.getFecha(usuario).call().then(e => {
 
@@ -197,12 +201,8 @@ app.post('/metodos', function (req, res) {
       });
       break;
     case "3":
-      
-      myContract.methods.getIdProfesor(usuario).call().then(e => {
-
-        if ((usuario ==prof[0]) || (usuario ==prof[1]) || (usuario ==prof[2])){
-          res.send('Usuario Invalido')
-        }
+      //--------------------------GET PROFESOR---------------------
+      myContract.methods.getIdProfesor(opc).call().then(e => {
         if (e.length < 1){
           res.send('Sin Tutorias Pendientes')
         }
@@ -215,9 +215,8 @@ app.post('/metodos', function (req, res) {
       });
       break;
     case "4":
-    
+      //---------------------------GET ALUMNO----------------------
       myContract.methods.getAlumno(opc).call().then(e => {
-
         var respuesta = 'Alumno: ';
         for (let index = 0; index < e.length; index++) {
           const a = e[index];
@@ -227,35 +226,37 @@ app.post('/metodos', function (req, res) {
       });
       break;
     case "5":
-      myContract.methods.confirmar(opc).send({from:usuario, gas:30000}).then(e => {
-        if (e.length < 1) {
-          res.send('Tutoria no encontrada')
-        }
-        var respuesta = 'Tutoria Confirmada';
-      //  respuesta += e
-        res.send(respuesta);
-      });
+      //----------------------------CONFIRMAR-----------------------
+      if ((usuario ==prof[0]) || (usuario ==prof[1]) || (usuario ==prof[2])){
+        myContract.methods.confirmar(opc).send({from:usuario, gas:200000}).then(e => {
+          if (e.length < 1) {
+            res.send('Tutoria no encontrada')
+          }
+          var respuesta = 'Tutoria Confirmada';
+        //  respuesta += e
+          res.send(respuesta);
+        });
+
+      }else{
+        res.send('Alumno no puede confirmar Materia')
+      }
       break;
     case "6":
-      myContract.methods.cancelar(usuario).send({from:usuario, gas:30000}).then(e => {
-
-        if ((usuario ==prof[0]) || (usuario==prof[1]) || (usuario ==prof[2])){
-          res.send('Profesores no pueden cancelar tutorias')
-        }
+      //-----------------------------CANCELAR--------------------------
+      if ((usuario in prof)){
+        res.send('Profesor no pueden solicitar materia')
+      }
+      myContract.methods.cancelar(opc).send({from:usuario, gas:200000}).then(e => {
         if (e.length < 1){
           res.send('Tutoria no encontrada')
         }
         var respuesta = 'Tutoria Cancelada';
-
-      //    respuesta += e;
         res.send(respuesta);
       });
       break;
     case "7":
-      myContract.methods.estaConfirmado(usuario).call().then(e => {
-        if ((usuario ==prof[0]) || (usuario==prof[1]) || (usuario ==prof[2])){
-          res.send('Usuario no valido')
-        }
+    //------------------------------ESTA CONFIRMADO---------------------
+      myContract.methods.estaConfirmado(opc).call().then(e => {
         if (e.length < 1) {
           res.send('Tutoria no encontrada')
         }
@@ -266,8 +267,8 @@ app.post('/metodos', function (req, res) {
       });
       break;
     case "8":
-      myContract.methods.estaCancelado(usuario).call().then(e => {
-
+    //-----------------------------ESTA CANCELADO------------------------
+      myContract.methods.estaCancelado(opc).call().then(e => {
         if (e.length < 1) {
           res.send('Tutoria no encontrada')
         }
@@ -277,6 +278,7 @@ app.post('/metodos', function (req, res) {
       });
       break;
     case "9":
+    //-----------------------------MIRAR TODAS LAS MATERIAS--------------
       if (usuario=="0xbd7b59c5c01e860150200df323b703228625eff7"){
         res.json(config.tutos)
       }
